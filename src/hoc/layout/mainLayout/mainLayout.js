502dfc3fe58466dda19a7ff184cd703c1ba0d86e
AlexUAKH <46876989+AlexUAKH@users.noreply.github.com>
2020-08-24 18:04:19 +0300

    material

--- src/hoc/layout/mainLayout/mainLayout.js
+++ src/hoc/layout/mainLayout/mainLayout.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react"
 import classes from "./mainLayout.module.css"
-import TopMenu from "../../../containers/topMenu"
 
 export default class MainLayout extends Component {
 
     render() {
         return (
             <div className={classes.mainLayout}>
-                <TopMenu />
+
                 <main>
                     { this.props.children }
                 </main>
