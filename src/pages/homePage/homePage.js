7f0c044ef39e1f22991ffd508bd10fc320e7bc85
MaksymMaslakov <kamsam243@gmail.com>
2020-08-28 19:49:52 +0300

    create ProductCard component

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,10 +1,22 @@
 import React from "react"
 import classes from "./homePage.css"
+import ProductCard from "../../components/product-card";
+import img from './../../illustration-geiranger.jpg'
 
 const HomePage = (props) => {
     return (
         <div className={ classes.homePage }>
-            <h1>HomePage</h1>
+            {/*<h1>HomePage</h1>*/}
+            <ProductCard product={{
+                name: 'Product name',
+                imageSrc: img,
+                cost: '23',
+                category: 'category',
+                stock: true,
+                description: ',mfkjsdnkjfg djfsgd fjj odsjf jj go sdfijg dsfgoisdf gdsfiojgoisdjf',
+                rating: 4
+            }}
+            />
         </div>
     )
 }
