047af734b2df70633c052f88cdbb675d8b1e5d00
MaksymMaslakov <kamsam243@gmail.com>
2020-08-28 22:37:30 +0300

    create Review component

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,22 +1,20 @@
 import React from "react"
 import classes from "./homePage.css"
-import ProductCard from "../../components/product-card";
-import img from './../../illustration-geiranger.jpg'
+import Review from "../../components/review";
+// import img from './../../illustration-geiranger.jpg'
 
 const HomePage = (props) => {
     return (
         <div className={ classes.homePage }>
             {/*<h1>HomePage</h1>*/}
-            <ProductCard product={{
-                name: 'Product name',
-                imageSrc: img,
-                cost: '23',
-                category: 'category',
-                stock: true,
-                description: ',mfkjsdnkjfg djfsgd fjj odsjf jj go sdfijg dsfgoisdf gdsfiojgoisdjf',
-                rating: 4
-            }}
-            />
+            <Review review={{
+                userName: 'Jhon',
+                date: '10/03/2018',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur\n' +
+                    '                unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam\n' +
+                    '                dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.',
+                rating: 3.5
+            }}/>
         </div>
     )
 }
